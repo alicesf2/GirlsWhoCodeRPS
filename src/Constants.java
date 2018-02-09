4a4119b27cd9a249837abcae8451aeaa48916b2b
Alice Fang <alicesf2@illinois.edu>
2018-02-08 23:29:56 -0600

    finished rock paper scissors game for Girls Who Code

---
+++ src/Constants.java
@@ -0,0 +1,17 @@
+/**
+ * class to hold the constants used throughout the game
+ */
+public class Constants {
+    public static final String ENTER_MOVE = "Enter your move, or type quit to exit:";
+    public static final String QUIT = "quit";
+    public static final String CPU_ROCK = "The CPU's move was rock.";
+    public static final String CPU_PAPER = "The CPU's move was paper.";
+    public static final String CPU_SCISSORS = "The CPU's move was scissors.";
+    public static final String TIE = "It's a tie!";
+    public static final String LOST = "You lost.";
+    public static final String WON = "You won!";
+    public static final String INVALID_MOVE = "Please enter a valid move.";
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+}
